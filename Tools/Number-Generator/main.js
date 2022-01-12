75f643d8cc80a2f9565a50d1eae467029136de6d
Gray <grayjacksonnoell@icloud.com>
2022-01-12 11:44:14 -0500

    update

--- Tools/Number-Generator/main.js
+++ Tools/Number-Generator/main.js
@@ -1,9 +1,13 @@
 var maxNum
 var numberNumber = 0
-var list = ["sus"]
+var list = []
 var number
 function start() {
-    maxNum = window.prompt("Maximum number");
+  if (document.getElementsByName("maxNum")[0].value !== ""){
+  maxNum = document.getElementsByName("maxNum")[0].value;
+  } else {
+    alert("ERROR: Please enter the required feilds.")
+  }
 }
 
 function getRandomInt(max) {
@@ -13,13 +17,16 @@ function getRandomInt(max) {
 function nextNumber() {
 if(numberNumber == maxNum) {
 alert("No more numbers to generate.");
+numberNumber = 0
+list = []
+maxNum = 0
 } else {
 numberNumber += 1
 number = getRandomInt(maxNum) + 1
 if (list.indexOf(number) !== -1){
   retry()
 } else {
-alert(number)
+  document.getElementById("output").innerHTML = "The number that was generated is: " + number
 list.push(number)
 number = 0
 }
@@ -30,7 +37,7 @@ function retry(){
 if (list.indexOf(number) !== -1){
   retry()
 } else {
-alert(number)
+document.getElementById("output").innerHTML = "The number that was generated is: " + number
 list.push(number)
 number = 0
 }}
