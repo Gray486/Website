8fdbc2b9ca857c7f99e886e4127ad1e7f0bfe107
Gray <grayjacksonnoell@icloud.com>
2022-01-13 14:34:21 -0500

    added d-w-t

--- Games/Dark-Web-Tycoon/main.js
+++ Games/Dark-Web-Tycoon/main.js
@@ -70,18 +70,6 @@ var allVariables = [
     nukeFactoryIncrease,
 ]
 
-function hideMenu() {
-  var x = document.getElementById("header");
-  var y = document.getElementById("hideMenu");
-  if (x.style.display === "none") {
-  x.style.display = "block";
-  y.style.borderBottom = "rgb(0, 168, 107) 6px none";
-  } else {
-    x.style.display = "none";
-    y.style.borderBottom = "rgb(0, 168, 107) 6px solid";
-  }
-}
-
 //Display All Numbers 
 
 //Runs when you click the bit coin and gives you money
